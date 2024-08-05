49579e509f53a090395e2c08e7bc9be2cf8197c7
Shardy678 <tambov.j678@gmail.com>
2024-08-05 11:49:49 +0300

    added flash functionality

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const flash = require('connect-flash')
 const path = require('path')
 const ejsMate = require('ejs-mate')
 const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override')
+const session = require('express-session')
 
 const campgrounds = require('./routes/campgrounds.js')
 const reviews = require('./routes/reviews.js')
@@ -24,6 +26,26 @@ app.set('views', path.join(__dirname,'views'))
 
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'))
+
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig))
+app.use(flash())
+
+ 
+app.use((req,res,next)=>{
+    res.locals.success = req.flash('success')
+    res.locals.error = req.flash('error')
+    next()
+})
 
 app.use('/campgrounds',campgrounds)
 app.use('/campgrounds/:id/reviews',reviews)
