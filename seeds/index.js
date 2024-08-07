dc88d8b04217ad351fc2592a96f206fe16bb78db
Shardy678 <tambov.j678@gmail.com>
2024-08-07 11:02:21 +0300

    review auth

--- seeds/index.js
+++ seeds/index.js
@@ -46,6 +46,7 @@ const seedDB = async () => {
             const random1000 = Math.floor(Math.random() * 1000);
             const price = Math.floor(Math.random() * 20) + 10;
             const camp = new Campground({
+                author: '66b23a2f78d093868dd8a00e',
                 location: `${cities[random1000].city}, ${cities[random1000].state}`,
                 title: `${sample(descriptors)} ${sample(places)}`,
                 image: await getRandomImage(),
