2e658ae9c6051fd80d4b527e4b98e3ebae8534d4
Shardy678 <tambov.j678@gmail.com>
2024-08-12 13:54:26 +0300

    update environment variables

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 const MongoDBStore = require('connect-mongo');
-const dbUrl = 'mongodb://localhost:27017/yelp-camp'
+const dbUrl = process.env.DB_URL
 // 'mongodb://localhost:27017/yelp-camp'
 // process.env.DB_URL
 
